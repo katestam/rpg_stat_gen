f0df89b42d9118223bfcd965a3da2e823bf1ba94
Katelyn Stamas <cakelyn@gmail.com>
2017-11-26 14:30:54 -0500

    re created client folder and removed gitignore for dist

---
+++ client/src/index.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Char_display from './components/char_display.jsx';
+import Form from './components/form.jsx';
+
+class Index extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      characters: [],
+      posted: false
+    };
+    this.post = this.post.bind(this);
+  }
+
+  get() {
+    var that = this;
+
+    $.ajax({
+      method: 'GET',
+      url: '/allChars'
+    }).then(function(data) {
+      that.setState({
+        characters: data
+      });
+    }).done();
+  }
+
+  post(data) {
+    var that = this;
+
+    $.ajax({
+      method: 'POST',
+      url: '/newChar',
+      data: JSON.stringify(data),
+      contentType: 'application/json'
+    }).then(this.get())
+    .done();
+  }
+
+  componentDidMount() {
+    this.get();
+  }
+
+  render() {
+
+    return (
+      <div>
+        <h1>RPG Character Generator</h1>
+        <Form post={this.post} />
+        <h4>Recently created characters...</h4>
+        <Char_display characters={this.state.characters}/>
+      </div>
+    )
+
+  }
+}
+
+ReactDOM.render(<Index />, document.getElementById('app'));
