cb73c1d2f4649f37b36095d62cfa5c13da040b74
Katelyn Stamas <cakelyn@gmail.com>
2017-11-26 19:10:26 -0500

    styled table

--- client/src/components/character.jsx
+++ client/src/components/character.jsx
@@ -1,37 +1,45 @@
 import React from 'react';
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn,
+} from 'material-ui/Table';
 
 var Character = function(props) {
   return (
 
     <div>
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Race</th>
-            <th>Class</th>
-            <th>Strength</th>
-            <th>Dexterity</th>
-            <th>Constitution</th>
-            <th>Intelligence</th>
-            <th>Wisdom</th>
-            <th>Charisma</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>{props.char.name}</td>
-            <td>{props.char.race}</td>
-            <td>{props.char.class}</td>
-            <td>{props.char.strength}</td>
-            <td>{props.char.dexterity}</td>
-            <td>{props.char.constitution}</td>
-            <td>{props.char.intelligence}</td>
-            <td>{props.char.wisdom}</td>
-            <td>{props.char.charisma}</td>
-          </tr>
-        </tbody>
-      </table>
+      <Table>
+        <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
+          <TableRow>
+            <TableHeaderColumn>Name</TableHeaderColumn>
+            <TableHeaderColumn>Race</TableHeaderColumn>
+            <TableHeaderColumn>Class</TableHeaderColumn>
+            <TableHeaderColumn>Strength</TableHeaderColumn>
+            <TableHeaderColumn>Dexterity</TableHeaderColumn>
+            <TableHeaderColumn>Constitution</TableHeaderColumn>
+            <TableHeaderColumn>Intelligence</TableHeaderColumn>
+            <TableHeaderColumn>Wisdom</TableHeaderColumn>
+            <TableHeaderColumn>Charisma</TableHeaderColumn>
+          </TableRow>
+        </TableHeader>
+        <TableBody displayRowCheckbox={false}>
+          <TableRow>
+            <TableRowColumn>{props.char.name}</TableRowColumn>
+            <TableRowColumn>{props.char.race}</TableRowColumn>
+            <TableRowColumn>{props.char.class}</TableRowColumn>
+            <TableRowColumn>{props.char.strength}</TableRowColumn>
+            <TableRowColumn>{props.char.dexterity}</TableRowColumn>
+            <TableRowColumn>{props.char.constitution}</TableRowColumn>
+            <TableRowColumn>{props.char.intelligence}</TableRowColumn>
+            <TableRowColumn>{props.char.wisdom}</TableRowColumn>
+            <TableRowColumn>{props.char.charisma}</TableRowColumn>
+          </TableRow>
+        </TableBody>
+      </Table>
     </div>
 
     )
