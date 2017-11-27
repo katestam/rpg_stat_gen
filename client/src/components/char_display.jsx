63e13fd319567205e7e8a6fa810ccebd7a4480b4
Katelyn Stamas <cakelyn@gmail.com>
2017-11-26 19:14:51 -0500

    made is so there is only one table header for everyone rather than a table header for each character

--- client/src/components/char_display.jsx
+++ client/src/components/char_display.jsx
@@ -1,13 +1,38 @@
 import React from 'react';
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn,
+} from 'material-ui/Table';
 import Character from './character.jsx';
 
 var Char_display = function(props) {
 
   return (
     <div>
+      <Table>
+        <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
+          <TableRow>
+            <TableHeaderColumn>Name</TableHeaderColumn>
+            <TableHeaderColumn>Race</TableHeaderColumn>
+            <TableHeaderColumn>Class</TableHeaderColumn>
+            <TableHeaderColumn>Strength</TableHeaderColumn>
+            <TableHeaderColumn>Dexterity</TableHeaderColumn>
+            <TableHeaderColumn>Constitution</TableHeaderColumn>
+            <TableHeaderColumn>Intelligence</TableHeaderColumn>
+            <TableHeaderColumn>Wisdom</TableHeaderColumn>
+            <TableHeaderColumn>Charisma</TableHeaderColumn>
+          </TableRow>
+        </TableHeader>
+        <TableBody displayRowCheckbox={false}>
       {props.characters.map(function(char) {
         return <Character char={char} />
       })}
+        </TableBody>
+      </Table>
     </div>
   )
 }
--- client/src/components/character.jsx
+++ client/src/components/character.jsx
@@ -10,38 +10,17 @@ import {
 
 var Character = function(props) {
   return (
-
-    <div>
-      <Table>
-        <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
-          <TableRow>
-            <TableHeaderColumn>Name</TableHeaderColumn>
-            <TableHeaderColumn>Race</TableHeaderColumn>
-            <TableHeaderColumn>Class</TableHeaderColumn>
-            <TableHeaderColumn>Strength</TableHeaderColumn>
-            <TableHeaderColumn>Dexterity</TableHeaderColumn>
-            <TableHeaderColumn>Constitution</TableHeaderColumn>
-            <TableHeaderColumn>Intelligence</TableHeaderColumn>
-            <TableHeaderColumn>Wisdom</TableHeaderColumn>
-            <TableHeaderColumn>Charisma</TableHeaderColumn>
-          </TableRow>
-        </TableHeader>
-        <TableBody displayRowCheckbox={false}>
-          <TableRow>
-            <TableRowColumn>{props.char.name}</TableRowColumn>
-            <TableRowColumn>{props.char.race}</TableRowColumn>
-            <TableRowColumn>{props.char.class}</TableRowColumn>
-            <TableRowColumn>{props.char.strength}</TableRowColumn>
-            <TableRowColumn>{props.char.dexterity}</TableRowColumn>
-            <TableRowColumn>{props.char.constitution}</TableRowColumn>
-            <TableRowColumn>{props.char.intelligence}</TableRowColumn>
-            <TableRowColumn>{props.char.wisdom}</TableRowColumn>
-            <TableRowColumn>{props.char.charisma}</TableRowColumn>
-          </TableRow>
-        </TableBody>
-      </Table>
-    </div>
-
+        <TableRow>
+          <TableRowColumn>{props.char.name}</TableRowColumn>
+          <TableRowColumn>{props.char.race}</TableRowColumn>
+          <TableRowColumn>{props.char.class}</TableRowColumn>
+          <TableRowColumn>{props.char.strength}</TableRowColumn>
+          <TableRowColumn>{props.char.dexterity}</TableRowColumn>
+          <TableRowColumn>{props.char.constitution}</TableRowColumn>
+          <TableRowColumn>{props.char.intelligence}</TableRowColumn>
+          <TableRowColumn>{props.char.wisdom}</TableRowColumn>
+          <TableRowColumn>{props.char.charisma}</TableRowColumn>
+        </TableRow>
     )
 }
 
